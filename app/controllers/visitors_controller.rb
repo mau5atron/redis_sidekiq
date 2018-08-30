03965748ad0f16adaf73de91174aae7916aaf079
mau5atron <gabrielbetancourt96@outlook.com>
2018-08-29 22:10:45 -0400

    configured routes / form / mail stuff / updated controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,17 @@
 class VisitorsController < ApplicationController
+	def index
+		
+	end
+
+	def contact
+		h = JSON.generate({
+			'name' => params[:name],
+			'email' => params[:email],
+			'message' => params[:message]
+		})
+
+		PostmanWorker.perform_async(h, 5)
+
+		redirect_to :root
+	end
 end
